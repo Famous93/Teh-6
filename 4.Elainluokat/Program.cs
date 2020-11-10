9517f7c3a3f5a7c4b9891ce8aef7891ec5147b3f
Famous93 <73938606+Famous93@users.noreply.github.com>
2020-11-10 15:12:04 +0200

    changes

---
+++ 4.Elainluokat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elainluokat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Koira ko = new Koira();
+            ko.name = "Max";
+            Console.WriteLine(ko.palautaKoirannimi());
+            Console.WriteLine("===========================");
+            
+            ko.asetaKoirannimi("musti");
+            Console.WriteLine(ko.palautaKoirannimi());
+
+            Console.ReadKey();
+        }
+    }
+}
